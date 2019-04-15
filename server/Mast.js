f13c35d8ef8d0b01ebc6b26cd05b2a2eb7f6ca9a
adrianblonski <adblonski1@gmail.com>
2019-04-15 13:13:01 +0200

    Added block physics

--- server/Mast.js
+++ server/Mast.js
@@ -13,15 +13,14 @@ class Point {
     }
 }
 
-
-module.exports = class Mast {
+class Mast {
     constructor(id, x, y) {
         this.id = id;
-        this.physicsObj = new engine.physicsObject(x - this.length, y - this.length);
-        this.gravityForce = new engine.vector(0, 0.4);
-        this.jumpForce = new engine.vector(0, -10);
+        this.physicsObj = new engine.PhysicsObject(x - this.length, y - this.length);
+        this.gravityForce = new engine.Vector(0, 0.4);
+        this.jumpForce = new engine.Vector(0, -10);
         this.length = 16;
-        this.physicsObj = new engine.physicsObject(x - this.length, y - this.length);
+        this.physicsObj = new engine.PhysicsObject(x - this.length, y - this.length);
         this.first_end = new Point(x - this.length,y - this.length);
         this.angle = 0.0;
         this.key_states = {
@@ -30,40 +29,39 @@ module.exports = class Mast {
             left:  false,
             right: false
         }
+    }
+
+    update(isHeld) {
+        //console.log(this.first_end.x+" "+this.first_end.y);
+
+        //@TODO limit mast not to fall beneath player's shoulder
+        this.physicsObj.applyForce(this.gravityForce);
+        //this.physicsObj.update();
+        this.first_end.y = this.physicsObj.pos.y;  // gravity is independent from player's movement
+        if(isHeld) {
+            this.first_end.x = this.physicsObj.pos.x;
+                    }
+        if(this.angle == 0) return;
+
+        if(this.angle < 0) {
+            this.angle += 0.033*(this.angle);
+            this.angle = Math.max(-90,this.angle)  //pull mast towards floor
         }
-
-
-
-        update(isHeld) {
-            //console.log(this.first_end.x+" "+this.first_end.y);
-
-            //@TODO limit mast not to fall beneath player's shoulder
-            this.physicsObj.applyForce(this.gravityForce);
-            this.physicsObj.update();
-            this.first_end.y = this.physicsObj.pos.y;  // gravity is independent from player's movement
-            if(isHeld) {
-                this.first_end.x = this.physicsObj.pos.x;
-                       }
-            if(this.angle == 0) return;
-
-            if(this.angle < 0) {
-                this.angle += 0.033*(this.angle);
-                this.angle = Math.max(-90,this.angle)  //pull mast towards floor
-            }
-            if(this.angle > 0){
-                this.angle += 0.033*(this.angle); // power of pull. The bigger the angle, the stronger is the pulling
-                this.angle = Math.min(90,this.angle) // pull mast towards floot
-            }
-           // console.log(this.angle+"\n"); //debug
-                }
-
-        get info() {
-            return {
-                x: this.first_end.x,
-                y: this.first_end.y,
-                angle: this.angle
-            };
-
+        if(this.angle > 0){
+            this.angle += 0.033*(this.angle); // power of pull. The bigger the angle, the stronger is the pulling
+            this.angle = Math.min(90,this.angle) // pull mast towards floot
         }
+        // console.log(this.angle+"\n"); //debug
+    }
+
+    get info() {
+        return {
+            x: this.first_end.x,
+            y: this.first_end.y,
+            angle: this.angle
+        };
 
     }
+}
+
+module.exports = Mast;
--- server/room.js
+++ server/room.js
@@ -1,4 +1,8 @@
-module.exports = class Room {
+var engine = require('./physics.js');
+var scale = 32;
+var block = [null, new engine.PhysicsObject(-1, -1, 0), new engine.PhysicsObject(-1, -1, 0.6)];
+
+class Room {
     constructor(slots, code) {
         // TODO: slots validation
         this.slots = slots;
@@ -9,16 +13,18 @@ module.exports = class Room {
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-                    [0, 0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2],
-                    [0, 0, 2, 1, 1, 2, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2],
-                    [0, 0, 2, 1, 1, 2, 0, 2, 1, 2, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
-                    [0, 0, 2, 2, 2, 2, 0, 2, 1, 2, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
-                    [0, 0, 2, 1, 1, 2, 0, 2, 1, 2, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
-                    [0, 0, 2, 1, 1, 2, 0, 2, 1, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2],
-                    [0, 0, 2, 1, 1, 2, 0, 2, 1, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2],
-                    [0, 0, 2, 1, 1, 2, 0, 2, 1, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]];
+    
+        this.gravityForce = new engine.Vector(0, 0.3);
     }
 
     get info() {
@@ -28,22 +34,52 @@ module.exports = class Room {
         };
     }
 
+    updateLevel() {
+        let room = this;
+        this.players.forEach(function(player) {
+            let x_l = Math.floor(player.physicsObj.pos.x / scale) % 19;
+            let x_r = Math.floor((player.physicsObj.pos.x + scale - 1) / scale) % 19;
+            let y = Math.floor((player.physicsObj.pos.y + 1) / scale + 1) % 15;
+
+            x_l = (x_l < 0) ? 0 : x_l;
+            x_r = (x_r < 0) ? 0 : x_r;
+            y = (y < 0) ? 0 : y;
+
+            if(room.map[y][x_l] == 0 && room.map[y][x_r] == 0) {
+                player.physicsObj.applyForce(room.gravityForce);
+                player.inAir = true;
+            } else {
+                player.physicsObj.pos.y = (y - 1) * scale;
+                if(block[room.map[y][x_l]])
+                    player.physicsObj.bounce(block[room.map[y][x_l]].ref);
+                else
+                    player.physicsObj.bounce(block[room.map[y][x_r]].ref);
+                
+                player.inAir = false;
+            }
+            
+            player.update();
+        });
+    }
+
     join(player) {
         this.players.push(player);
         console.log("Player [" + player.id + "] has joined room '" + this.code + "'");
     }
 
     disconnect(player_id) {
-        if(!this.find_player(player_id)) return;
+        if(!this.findPlayer(player_id)) return;
         this.players = this.players.filter(player => player.id != player_id);
     }
 
     update(player_id, data) {
-        if(!this.find_player(player_id)) return;
-        this.find_player(player_id).key_states = data;
+        if(!this.findPlayer(player_id)) return;
+        this.findPlayer(player_id).key_states = data;
     }
 
-    find_player(player_id) {
+    findPlayer(player_id) {
         return this.players.find(player => player.id == player_id);
     }
 }
+
+module.exports = Room;
