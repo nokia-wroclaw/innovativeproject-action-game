6bb65cea89d22d6e066f4025aaa577a3dbde7a13
tomkowski <siemtomek@gmail.com>
2019-06-03 12:03:13 +0200

    working bat. No animation

--- server/player.js
+++ server/player.js
@@ -2,7 +2,9 @@ var engine = require('./physics.js');
 var Mast = require('./mast.js');
 
 class Player {
-    constructor(id, x, y, speed = 1.5) {
+    constructor(id, x, y, speed = 3.0) {
+        this.height = 64;
+        this.width = 32;
         this.id = id;
         this.speed = speed;
         this.face = 0;
@@ -51,6 +53,17 @@ class Player {
         this.physicsObj.update();
          // TYMCZASOWO
 
+        if(this.physicsObj.pos.x < 0){
+            this.physicsObj.pos.x += 5;
+            this.physicsObj.vel.x *= -1;
+        }
+        else
+        if(this.physicsObj.pos.x > 608){
+            this.physicsObj.pos.x -= 5;
+            this.physicsObj.vel.x *= -1;
+
+        }
+
         if(this.key_states.space && !this.cooldown){
             //this.pickUpMast(); //checks for mast update and picks it up if possible
             this.cooldown = true;
@@ -73,7 +86,7 @@ class Player {
         }
         if (this.key_states.right) {
             this.face = 1; // to animate
-            if(this.physicsObj.pos.x  < 608)
+            if(this.physicsObj.pos.x  < 608)  // 608 to static field (MAP_WIDTH)
             {this.physicsObj.pos.x += this.speed;
                 this.mast.physicsObj.pos.x += this.speed;
                 this.mast.angle -= this.speed * ((90.0 - this.mast.angle)/4) * 0.03; //while walking right decrease the angle
